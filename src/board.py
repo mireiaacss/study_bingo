700d2b21adf49df6984f82158d166b581a8ed60a
anaarroyo8 <160605649+anaarroyo8@users.noreply.github.com>
2025-10-29 17:53:15 +0100

    Updated coordinates for bingo board

--- src/board.py
+++ src/board.py
@@ -1,5 +1,6 @@
 # src/board.py 
 import random 
+
  
 def create_number_dataset(start=1, end=99):
     """Return a list of numbers available for the bingo."""
@@ -12,7 +13,16 @@ def generate_bingo_card():
     card = [chosen[i*9:(i+1)*9] for i in range(3)]  # Split into 3 rows of 9
     return card
 
+def print_bingo_card(card):
+    column_labels = ['A','B','C','D','E','F','G','H','I']
+    print("   " + "  ".join(column_labels))
+    print("  " + "---" * len(column_labels))
+
+    for i, row in enumerate(card, start=1):
+        formatted_row = "  ".join(f"{num:2d}" for num in row)
+        print(f"{i} | {formatted_row}")
+
+
 if __name__ == "__main__":
     card = generate_bingo_card()
-    for row in card:
-        print(row)
+    print_bingo_card(card)
